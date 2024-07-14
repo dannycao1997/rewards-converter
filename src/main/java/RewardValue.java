89df7786612a93848193ff346633db0f8cba198d
Danny Cao <dannycao1997@yahoo.com>
2024-07-14 03:19:04 -0400

    create constructor and methods for RewardValue

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -2,5 +2,29 @@ public class RewardValue {
 
     private double cashValue;
     private double milesValue;
+    private static final double MILES_TO_CASH_RATE = 0.0035;
 
+    // constructor that accepts cash value
+    public RewardValue(double cashValue){
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / MILES_TO_CASH_RATE;
+    }
+
+    // constructor that accepts miles value
+    public RewardValue(double milesValue, boolean isMiles){
+        if (isMiles){
+            this.milesValue = milesValue;
+            this.cashValue = milesValue * MILES_TO_CASH_RATE;
+        }
+    }
+
+    // method to get cash value
+    public double getCashValue(){
+        return cashValue;
+    }
+
+    // method to get miles value
+    public double getMilesValue(){
+        return milesValue;
+    }
 }
